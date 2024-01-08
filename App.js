1ec23b0ed4c9bf6489ebb2d13e6ce08e638bebda
harjasilin <harjasilin.01@gmail.com>
2024-01-08 10:18:45 +0530

    first

---
+++ App.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+function App() {
+
+
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
